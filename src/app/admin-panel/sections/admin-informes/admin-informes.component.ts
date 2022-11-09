423fdc1da91745dde3a3c1972ad38c744e3beb69
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-11-09 00:37:37 -0300

    Feat carga de informe

--- src/app/admin-panel/sections/admin-informes/admin-informes.component.ts
+++ src/app/admin-panel/sections/admin-informes/admin-informes.component.ts
@@ -6,25 +6,32 @@ import { AlertService } from 'src/app/shared/services/alert.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { Horario } from 'src/app/models/horario.model';
 import { Punto } from 'src/app/models/puntos.model';
+import { Turno } from 'src/app/models/turno.model';
 @Component({
   selector: 'met-admin-informes',
   templateUrl: './admin-informes.component.html',
   styleUrls: ['./admin-informes.component.scss']
 })
-export class AdminInformesComponent  extends FormValidator implements OnInit {
+export class AdminInformesComponent extends FormValidator implements OnInit {
   public formGroup: FormGroup;
-  loading:boolean = false;
+  loading: boolean = false;
   puntos: Punto[];
   horarios: Horario[];
+  informes: any[];
+  showInformesAlta:boolean = true;
 
   constructor(private FB: FormBuilder, private alertSVC: AlertService, private storageSVC: StorageService) {
-    super()
+    super();
   }
 
   ngOnInit(): void {
     this.initForm();
-    this.getPuntos()
-    this.getHorarios();
+    this.getPuntos();
+    this.getInformes();
+  }
+
+  toggleShow(){
+    this.showInformesAlta = !this.showInformesAlta;
   }
 
   getPuntos() {
@@ -33,32 +40,65 @@ export class AdminInformesComponent  extends FormValidator implements OnInit {
     });
   }
 
-  getHorarios() {
-    this.storageSVC.GetAll('horarios').subscribe((horarios) => {
-      this.horarios = horarios;
+  onChangePunto() {
+    this.getHorarios(this.formGroup.value.punto);
+  }
+
+  getHorarios(punto) {
+    this.storageSVC.GetByParameter('turnos', 'punto', punto).subscribe((data) => {
+      this.horarios = [];
+      this.turnoToHorario(data);
+      console.log(data);
     });
   }
 
+  turnoToHorario(turno: Turno[]) {
 
-  saveInforme(){
-    this.loading = true;
-    setTimeout(() => {
-      this.loading = false;
-    }, 2000);
+    turno.forEach((element) => {
+      this.horarios.push(element.horario.horario);
+    });
   }
 
-  initForm(){
+  getInformes() {
+    this.storageSVC.GetAll('informes').subscribe((informes) => {
+      this.informes = informes;
+    });
+  }
+
+  saveInforme() {
+    this.loading = true;
+    let informe: any = {};
+    informe.turno = {
+      punto: this.formGroup.value.punto,
+      horario: this.formGroup.value.horario,
+      fecha: this.formGroup.value.fecha
+    };
+    informe.publicaciones = this.formGroup.value.publicaciones;
+    informe.videos = this.formGroup.value.videos;
+    console.log(informe);
+    this.storageSVC
+      .Insert('informes', informe)
+      .then((data) => {
+        this.alertSVC.alertCenter('success', 'Informe guardado con éxito');
+        this.loading = false;
+        this.formGroup.reset()
+        informe = {}
+      })
+      .catch((error) => {
+        this.alertSVC.alertCenter('error', 'Error al guardar el informe');
+        this.loading = false;
+      });
+  }
+
+  initForm() {
     this.formGroup = this.FB.group({
       punto: ['', [Validators.required]],
       horario: ['', [Validators.required]],
-      fecha: ['', []],
-      publicaciones: ['', []],
-      videos: ['', []],
+      fecha: ['', [Validators.required]],
+      publicaciones: ['', [Validators.required]],
+      videos: ['', [Validators.required]],
     });
   }
 
-  definirMensajesError(): void {
-      
-  }
-
+  definirMensajesError(): void {}
 }
